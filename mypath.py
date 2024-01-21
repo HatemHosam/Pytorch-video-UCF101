c9e3a78b49b86beb0bd13f7bb722577a76396e9c
Hatem Ibrahem <48507516+HatemHosam@users.noreply.github.com>
2024-01-21 11:14:22 +0900

    Update mypath.py

--- mypath.py
+++ mypath.py
@@ -3,10 +3,10 @@ class Path(object):
     def db_dir(database):
         if database == 'ucf101':
             # folder that contains class labels
-            root_dir = '/Path/to/UCF-101'
+            root_dir = '/data/i5O/UCF-101'
 
             # Save preprocess data into output_dir
-            output_dir = '/path/to/VAR/ucf101'
+            output_dir = '/data/i5O/post_process/UCF-101'
 
             return root_dir, output_dir
         elif database == 'hmdb51':
@@ -16,10 +16,17 @@ class Path(object):
             output_dir = '/path/to/VAR/hmdb51'
 
             return root_dir, output_dir
+        elif database == 'kinetics400':
+            # folder that contains class labels
+            root_dir = '/data/i5O/k400/videos'
+
+            output_dir = '/data/i5O/post_process/kinetics400'
+
+            return root_dir, output_dir
         else:
             print('Database {} not available.'.format(database))
             raise NotImplementedError
 
     @staticmethod
     def model_dir():
-        return '/path/to/Models/c3d-pretrained.pth'
+        return 'Models/c3d-pretrained.pth'
